8784497640cfdb1e57314374b0ab5ca93c1d6c8b
Kai Raphahn <kai.raphahn@laburec.de>
2020-02-08 02:02:31 +0100

    feat: Write data into CSV file
    write text writing
    
    Issue #8

--- easyb/data/text.py
+++ easyb/data/text.py
@@ -16,7 +16,11 @@
 #    Copyright (C) 2017, Kai Raphahn <kai.raphahn@laburec.de>
 #
 
-from easyb.data.base import Storage, Collection
+import os
+import easyb
+
+from easyb.data.base import Storage, Collection, convert_data
+from io import FileIO
 
 __all__ = [
     "TextStorage"
@@ -27,8 +31,80 @@ class TextStorage(Storage):
 
     # noinspection PyTypeChecker
     def __init__(self, data: Collection):
+        self.file: FileIO = None
         Storage.__init__(self, "TEXT", data)
         return
 
+    def _prepare(self) -> bool:
+        filename = os.path.abspath(os.path.normpath(self.data.filename + ".csv"))
+        easyb.log.inform(self.name, "Open {0:s}".format(filename))
+        try:
+            self.file = open(filename, mode="w")
+        except OSError as e:
+            easyb.log.exception(e)
+            return False
+        return True
+
+    def _write_info(self):
+
+        self.file.write("Name\tValue\n")
+
+        for item in self.data.infos:
+            line = "{0:s}\t{1:s}\n".format(item.name, str(item))
+            self.file.write(line)
+
+        self.file.write("\n")
+        return
+
+    def _write_status(self):
+
+        self.file.write("State\tValue\n")
+
+        for item in self.data.status:
+            line = "{0:s}\t{1:s}\n".format(item.name, str(item))
+            self.file.write(line)
+
+        self.file.write("\n")
+        return
+
+    def _write_data(self):
+
+        line = ""
+        for column in self.data.columns:
+            if line == "":
+                line = column.description
+            else:
+                line += "\t{0:s}".format(column.description)
+
+        line += "\n"
+        self.file.write(line)
+
+        for row in self.data.rows:
+
+            line = ""
+            for column in self.data.columns:
+                data = getattr(row, column.name)
+                value = convert_data(column.type, data)
+
+                if line == "":
+                    line = value
+                else:
+                    line += "\t{0:s}".format(value)
+
+            line += "\n"
+            self.file.write(line)
+
+        return
+
     def store(self) -> bool:
+        check = self._prepare()
+        if check is False:
+            return False
+
+        self._write_info()
+        self._write_status()
+        self._write_data()
+
+        if self.file is not None:
+            self.file.close()
         return True
